873dc3ac264c6c6070ef7d728b9f75c9888c483f
Dmitry Kozhukhar <dna.coj@gmail.com>
2019-06-13 00:33:00 +0300

    9. Try not to use Truth module. Because of conflict in dependencies (com.google.guava)

--- kotlin-coroutines-repository/app/src/androidTest/java/com/example/android/kotlincoroutines/main/FakeNetworkCallAwaitTest.kt
+++ kotlin-coroutines-repository/app/src/androidTest/java/com/example/android/kotlincoroutines/main/FakeNetworkCallAwaitTest.kt
@@ -19,6 +19,9 @@ package com.example.android.kotlincoroutines.main
 import com.example.android.kotlincoroutines.main.fakes.makeFailureCall
 import com.example.android.kotlincoroutines.main.fakes.makeSuccessCall
 import com.example.android.kotlincoroutines.util.FakeNetworkException
+import com.example.android.kotlincoroutines.util.FakeNetworkSuccess
+import com.google.common.truth.Truth
+import kotlinx.coroutines.runBlocking
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.junit.runners.JUnit4
@@ -29,14 +32,16 @@ class FakeNetworkCallAwaitTest {
     @Test
     fun whenFakeNetworkCallSuccess_resumeWithResult() {
         val subject = makeSuccessCall("the title")
+        runBlocking { assert( subject.await() == "the title") }
 
-        // TODO: Implement test for await success
     }
 
     @Test(expected = FakeNetworkException::class)
     fun whenFakeNetworkCallFailure_throws() {
         val subject = makeFailureCall(FakeNetworkException("the error"))
 
-        // TODO: Implement test for await failure
+        runBlocking {
+            subject.await()
+        }
     }
 }
